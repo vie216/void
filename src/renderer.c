2e38dd16d8c064d2a2a3352544283ab7343bf1f4
vie <vie.devr@gmail.com>
2024-02-02 00:05:47 +0300

    Add scrolling

--- src/renderer.c
+++ src/renderer.c
@@ -5,31 +5,39 @@
 #include "renderer.h"
 #include "config.h"
 
-void renderer_render_line(Renderer *renderer, Line *line, u32 _row) {
+void renderer_render_line(Renderer *renderer, Buffer *buffer, u32 _row) {
+  if (_row + renderer->scroll >= buffer->len)
+    return;
+
   LineInfo *info = renderer->line_infos + _row;
+  Line *line = buffer->items + _row + renderer->scroll;
   u32 height = 1;
   u32 col = 0;
 
   while (col < line->len && col < renderer->cap) {
+    u32 offset = _row * renderer->cols + col;
+
     if (col != 0 && col % renderer->cols == 0)
       height++;
 
     if (line->items[col] == '\t') {
       if (renderer->cap - col > TAB_WIDTH)
-        sprintf(renderer->buffer + _row * renderer->cols + col, TAB_STR);
+        sprintf(renderer->buffer + offset, TAB_STR);
       col += TAB_WIDTH;
     } else {
-      if (line->items[col] != renderer->buffer[_row * renderer->cols + col]) {
+      if (line->items[col] != renderer->buffer[offset]) {
         info->dirty = true;
-        renderer->buffer[_row * renderer->cols + col] = line->items[col];
+        renderer->buffer[offset] = line->items[col];
       }
       col++;
     }
   }
 
   if (line->len < info->len) {
+    u32 offset = _row * renderer->cols + line->len;
+
     info->dirty = true;
-    memset(renderer->buffer + _row * renderer->cols + line->len,
+    memset(renderer->buffer + offset,
            ' ', info->len - line->len);
   }
 
@@ -81,10 +89,23 @@ void renderer_render_buffer(Renderer *renderer, Buffer *buffer) {
     full_redraw = true;
   }
 
+  if (buffer->row < renderer->scroll) {
+    renderer->scroll = buffer->row;
+    full_redraw = true;
+#ifdef NDEBUG
+  } else if (buffer->row > renderer->scroll + renderer->rows - 2) {
+    renderer->scroll = buffer->row - renderer->rows + 2;
+#else
+  } else if (buffer->row > renderer->scroll + renderer->rows - 3) {
+    renderer->scroll = buffer->row - renderer->rows + 3;
+#endif
+    full_redraw = true;
+  }
+
   fputs("\033[H", stdout);
   for (u32 row = 0; row + 1 < renderer->rows && row < buffer->len; ++row) {
     if (buffer->dirty || full_redraw)
-      renderer_render_line(renderer, buffer->items + row, row);
+      renderer_render_line(renderer, buffer, row);
 
     if (row != 0)
       putc('\n', stdout);
@@ -117,7 +138,7 @@ void renderer_render_buffer(Renderer *renderer, Buffer *buffer) {
 #endif
 
   printf("\033[%d;%dH",
-         buffer->row + 1,
+         buffer->row - renderer->scroll + 1,
          buffer_visual_col(buffer) + 1);
   fflush(stdout);
 }
--- src/renderer.h
+++ src/renderer.h
@@ -12,9 +12,10 @@ typedef struct {
   LineInfo *line_infos;
   u32       rows, cols, cap;
   u32       prev_buffer_len;
+  u32       scroll;
 } Renderer;
 
-void renderer_render_line(Renderer *renderer, Line *line, u32 _row);
+void renderer_render_line(Renderer *renderer, Buffer *buffer, u32 _row);
 #ifndef NDEBUG
 void renderer_render_debug_info(Renderer *renderer, bool full_redraw);
 #endif
