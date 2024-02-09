97ef4f98a1fa5d9e8a9360289e85d3c755df7b4d
vie <vie.devr@gmail.com>
2024-02-09 23:37:07 +0300

    Fix fucky wacky segfaulty (attempt 2)

--- src/defs.c
+++ src/defs.c
@@ -5,10 +5,10 @@ void da_reserve_space(void **items, u32 *len, u32 *cap, u32 size, u32 amount) {
     if (*cap != 0) {
       while (*cap < *len + amount)
         *cap *= 2;
-      items = realloc(items, size * *cap);
+      *items = realloc(*items, size * *cap);
     } else {
       *cap = 1;
-      items = malloc(size * *cap);
+      *items = malloc(size * *cap);
     }
   }
 }
