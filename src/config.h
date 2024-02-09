df2abef22509dcb073bb4fadeaedd88af1378f09
vie <vie.devr@gmail.com>
2024-02-09 15:41:02 +0300

    Change config.h

--- src/config.h
+++ src/config.h
@@ -1,10 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include "defs.h"
+#include <stdbool.h>
 
-#define TAB_STR   "  "
-#define TAB_WIDTH (sizeof(TAB_STR) - 1)
+#define TAB_WIDTH 2
 #define HARD_TABS false
 
 #endif // CONFIG_H
